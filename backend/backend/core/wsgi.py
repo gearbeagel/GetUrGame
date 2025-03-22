a42f7cb8ec7502ebb22e9f00c4414c8e131486f0
Vic K <kondratskayavictoria@gmail.com>
2025-03-22 22:32:58 +0200

    FEAT: added docker

--- backend/backend/core/wsgi.py
+++ backend/backend/core/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from configurations.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
 application = get_wsgi_application()
